2e844c797057e8f278d94a2b2be2f5d2763e80f4
BKR70 <bilas.duet.204027@gmail.com>
2025-07-09 23:21:18 +0600

    Add Initial Migration

--- RMS.Infrastructure/Data/ApplicationDbContext.cs
+++ RMS.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using RMS.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,33 @@ namespace RMS.Infrastructure.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
         { 
         
+        }
+        public DbSet<Villa> Villas { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Villa>().HasData(
+                    new Villa
+                    {
+                        Id = 1,
+                        Name = "Royal Villa",
+                        Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                        ImageUrl = "https://placehold.co/600x400",
+                        Occupancy = 4,
+                        Price = 200,
+                        Sqft = 550,
+                    },
+                    new Villa
+                    {
+                        Id = 2,
+                        Name = "Premium Pool Villa",
+                        Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                        ImageUrl = "https://placehold.co/600x401",
+                        Occupancy = 4,
+                        Price = 300,
+                        Sqft = 550,
+                    }
+                    );
         }
     }
 }
